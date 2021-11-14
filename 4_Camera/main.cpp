f227717899f7cf23d6744ae9971d1a16223a38f8
zilong <zilong919@163.com>
2021-11-14 16:40:51 +0800

    Create gen project.

--- 4_Camera/main.cpp
+++ 4_Camera/main.cpp
@@ -75,7 +75,7 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader(R"(D:\ws\cpp\opengl\camera\vertexShader.vs)", R"(D:\ws\cpp\opengl\camera\fragmentShader.fs)");
+    Shader ourShader(R"(D:\ws\cpp\opengl\4_Camera\vertexShader.vs)", R"(D:\ws\cpp\opengl\4_Camera\fragmentShader.fs)");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -168,7 +168,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load(R"(D:\ws\cpp\opengl\camera\container.jpg)", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load(R"(D:\ws\cpp\opengl\4_Camera\container.jpg)", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -190,7 +190,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load(R"(D:\ws\cpp\opengl\camera\awesomeface.png)", &width, &height, &nrChannels, 0);
+    data = stbi_load(R"(D:\ws\cpp\opengl\4_Camera\awesomeface.png)", &width, &height, &nrChannels, 0);
     if (data)
     {
         // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
---
+++ 5_ShallowEncapsulate/src/Window/Windown.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <functional>
+#include <GLFW/glfw3.h>
+#include "OpenGLContext.h"
+#include <string>
+
+namespace gen {
+
+	struct WindowProps
+	{
+		std::string Title;
+		uint32_t Width;
+		uint32_t Height;
+
+		WindowProps(const std::string& title = "Hazel Engine",
+			uint32_t width = 1600,
+			uint32_t height = 900)
+			: Title(title), Width(width), Height(height)
+		{
+		}
+	};
+	
+	class Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+		
+		Window(const WindowProps& props);
+		virtual ~Window();
+
+		void OnUpdate();
+
+		unsigned int GetWidth() const { return m_Data.Width; }
+		unsigned int GetHeight() const{ return m_Data.Height; }
+
+		// Window attributes
+		void SetEventCallback(const EventCallbackFn& callback) { m_Data.EventCallback = callback; }
+		void SetVSync(bool enabled);
+		bool IsVSync() const;
+
+		virtual void* GetNativeWindow() const { return m_Window; }
+	private:
+		virtual void Init(const WindowProps& props);
+		virtual void Shutdown();
+	private:
+		GLFWwindow* m_Window;
+		std::unique_ptr<GraphicsContext> m_Context;
+
+		struct WindowData
+		{
+			std::string Title;
+			unsigned int Width, Height;
+			bool VSync;
+
+			EventCallbackFn EventCallback;
+		};
+
+		WindowData m_Data;
+	};
+
+}
